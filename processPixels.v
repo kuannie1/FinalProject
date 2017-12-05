ade83b4a9e28fc76ef8042a901e8e1c8b70ce9cb
Joseph Lee <leejos17@gmail.com>
2017-12-04 21:02:17 -0500

    fixed some bugs

--- processPixels.v
+++ processPixels.v
@@ -7,8 +7,8 @@
 */ 
 
 module processPixels #(
-	screenWidth = 640,
-	screenHeight = 480
+	parameter screenWidth = 640,
+	parameter screenHeight = 480
 	)
 (
 	input [9:0] xCoord,
@@ -18,11 +18,6 @@ module processPixels #(
 	input [7:0] blue,
 	output [2:0][screenWidth-1:0][screenHeight-1:0] screenArray
 );
-
-// parameter screenWidth = 640;
-// parameter screenHeight = 480;
-
-reg [2:0][screenWidth-1:0][screenHeight-1:0] screenArray;
 
 for (i = 0; i < $size(xCoord); i = i + 1) begin
 	screenArray[0] [xCoord[i]] [yCoord[i]] <= red[i];
